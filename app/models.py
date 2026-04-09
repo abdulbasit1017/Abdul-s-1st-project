a0db8065155a45323ec5f8a5fdff9e9facbe019c
Abdul Basit <abudlbasitgmail118@gmail.com>
2026-04-09 13:40:35 +0500

    Added description field to Product model and created migration

--- app/models.py
+++ app/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField()
     price = models.IntegerField() 
+    description = models.TextField(null=True, blank=True)
