349dbcc7521b3eefed5be07fac298940e37a45e8
Abdul Basit <abudlbasitgmail118@gmail.com>
2026-04-10 14:25:22 +0500

    Fix settings and serializers

--- app/serializers.py
+++ app/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than 0")
+        return value
     class Meta:
         model = Product
         fields = '__all__'
+
+    
