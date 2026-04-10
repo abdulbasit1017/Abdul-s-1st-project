6270df8ce0ee2317851c17c2690a7efb21cc79a3
Abdul Basit <abudlbasitgmail118@gmail.com>
2026-04-10 10:40:50 +0500

    Added STATIC_ROOT and requirements.txt

--- estore/settings.py
+++ estore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-yu6bd=0)rgwy)^qm@1b96sqy1a^$%7y)-65rh!(xtkgb7_fq)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
