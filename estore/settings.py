15dbe3b58fbaa7395f1df9ed669c650fad1de7a1
Abdul Basit <abudlbasitgmail118@gmail.com>
2026-04-10 10:53:38 +0500

    Update settings for Render (SECRET_KEY, DEBUG)

--- estore/settings.py
+++ estore/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yu6bd=0)rgwy)^qm@1b96sqy1a^$%7y)-65rh!(xtkgb7_fq)b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
