88a8ae959ba42570397dabb7be6e5926c60699e6
Abdul Basit <abudlbasitgmail118@gmail.com>
2026-04-13 08:45:42 +0500

    Updated serializers and settings

--- app/serializers.py
+++ app/serializers.py
@@ -6,6 +6,15 @@ class ProductSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError("Price must be greater than 0")
         return value
+     # 🔹 Name length validation
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Name must be at least 3 characters long")
+
+        if len(value) > 50:
+            raise serializers.ValidationError("Name must not exceed 50 characters")
+
+        return value
     class Meta:
         model = Product
         fields = '__all__'
